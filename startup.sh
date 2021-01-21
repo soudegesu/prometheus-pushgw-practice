f905e7ed0c94ebcad5af8db7d624c00ac0567428
soudegesu <soudegesu@hotmail.co.jp>
2021-01-21 13:58:27 +0900

    pull to push gateway

--- startup.sh
+++ startup.sh
@@ -1,18 +1,17 @@
 # !/bin/bash
 
 kubectl create namespace monitoring
-kubectl create -f clusterRole.yml
-kubectl create -f prometheus-configmap.yml -n monitoring
-
-# Deploy prometheus
-kubectl create -f prometheus-deployment.yml -n monitoring
-kubectl create -f prometheus-service.yml -n monitoring
-
-# Deploy grafana
-kubectl create -f grafana-deployment.yml -n monitoring
-kubectl create -f grafana-service.yml -n monitoring
+kubectl apply -f clusterRole.yml
+kubectl apply -f prometheus-configmap.yml -n monitoring
 
 # Deploy pushgateway
-kubectl create -f pushgateway-deployment.yml -n monitoring
-kubectl create -f pushgateway-service.yml -n monitoring
+kubectl apply -f pushgateway-deployment.yml -n monitoring
+kubectl apply -f pushgateway-service.yml -n monitoring
 
+# Deploy prometheus
+kubectl apply -f prometheus-deployment.yml -n monitoring
+kubectl apply -f prometheus-service.yml -n monitoring
+
+# Deploy grafana
+kubectl apply -f grafana-deployment.yml -n monitoring
+kubectl apply -f grafana-service.yml -n monitoring
