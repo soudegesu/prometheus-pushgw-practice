db8dfaef46b2106de69a0400a54f127f2684d16c
soudegesu <soudegesu@hotmail.co.jp>
2021-01-21 13:22:06 +0900

    add pushgateway

--- startup.sh
+++ startup.sh
@@ -3,7 +3,16 @@
 kubectl create namespace monitoring
 kubectl create -f clusterRole.yml
 kubectl create -f prometheus-configmap.yml -n monitoring
+
+# Deploy prometheus
 kubectl create -f prometheus-deployment.yml -n monitoring
 kubectl create -f prometheus-service.yml -n monitoring
 
+# Deploy grafana
 kubectl create -f grafana-deployment.yml -n monitoring
+kubectl create -f grafana-service.yml -n monitoring
+
+# Deploy pushgateway
+kubectl create -f pushgateway-deployment.yml -n monitoring
+kubectl create -f pushgateway-service.yml -n monitoring
+
