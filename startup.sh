98f8abc037f9eb808288b579f4b9ee107ccd6f61
soudegesu <soudegesu@hotmail.co.jp>
2021-01-22 09:23:46 +0900

    add default datasource

--- startup.sh
+++ startup.sh
@@ -2,16 +2,17 @@
 
 kubectl create namespace monitoring
 kubectl apply -f clusterRole.yml
-kubectl apply -f prometheus-configmap.yml -n monitoring
 
 # Deploy pushgateway
 kubectl apply -f pushgateway-deployment.yml -n monitoring
 kubectl apply -f pushgateway-service.yml -n monitoring
 
 # Deploy prometheus
+kubectl apply -f prometheus-configmap.yml -n monitoring
 kubectl apply -f prometheus-deployment.yml -n monitoring
 kubectl apply -f prometheus-service.yml -n monitoring
 
 # Deploy grafana
+kubectl apply -f grafana-configmap.yml -n monitoring
 kubectl apply -f grafana-deployment.yml -n monitoring
 kubectl apply -f grafana-service.yml -n monitoring
